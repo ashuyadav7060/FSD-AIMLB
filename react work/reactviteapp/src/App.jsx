eefba4e9e7cdc4ce3e6fabdb6ddc43e440886c41
ashuyadav7060 <ashuyadav411ash@gmail.com>
2025-02-20 12:09:20 +0530

    20feb

--- react work/reactviteapp/src/App.jsx
+++ react work/reactviteapp/src/App.jsx
@@ -33,3 +33,57 @@ function App() {
 }
 
 export default App
+
+
+
+import React from 'react'
+import Student from './Student';
+
+import StudentState from './StudentState';
+import Imagemanipulation from "./imagemanipulation"
+
+function App() {
+    const studentdata={}
+  return (
+    // <div style={{color:'Red'}}><h2>ABES Engineering College</h2>
+    // <div style={mystyle}>{a}</div>
+    <div style={{marginLeft:"300px"}}>
+      <Imagemanipulation/>
+      {/* <Student data={studentdata} /> */}
+      {/* <StudentState /> */}
+      {/* <div>
+          <Student name="Sahil Jaiswal" roll='2200321530143' college="Abes Engineeering College" branch="CSE-AIML" 
+           pic={<img src={logo} height={100} width={40}/>} 
+           />
+      </div> */}
+    </div>
+  
+  )
+}
+
+// export default App
+import React, { useState } from 'react'
+import Registration from './registration'
+// import registration from './registration'
+import Login from './login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+function App() {
+  const [shareData , setShareData]=useState();
+  return (
+
+    <div>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/login' element={<Login />}   />
+      <Route path='/reg' element={<Login />}   />
+
+
+      </Routes>
+      </BrowserRouter>
+      <Registration regData={setShareData} />
+      
+    </div>
+  )
+}
+export default App
